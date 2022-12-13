662714d842cf78c48fa8d17fe74bda0b452269bc
Lizter <lizterdsouza@gmail.com>
2022-12-14 00:26:09 +0530

    code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        RWOnly obj= new RWobj();
+        obj.setname("Rauch");
+        System.out.println(obj.getName);
+    }
   
 }
